948aeae9d8a61c37fb62642f3a1cdfda2eecf772
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-01-27 19:23:00 +0530

    Add files via upload

---
+++ if-else/Ifelse2.java
@@ -0,0 +1,13 @@
+class Ifelse2
+{
+public static void main(String agrs[])
+{
+String name="Upasna";
+if(name=="Tia")
+{
+System.out.println("Your name is Tia");
+}
+else
+{
+System.out.println("Your name is Upasna");
+}}}
---
+++ if-else/Ifelse3.java
@@ -0,0 +1,13 @@
+class Ifelse3
+{
+public static void main(String agrs[])
+{
+String city="Ghaziabad, UP";
+if(city.contains("Goa"))
+{
+System.out.println("You're living in Goa");
+}
+else
+{
+System.out.println("You're living in Ghaziabad");
+}}}
