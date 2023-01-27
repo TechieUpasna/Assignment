154601cf8f014447687d105a950e8d945588c770
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-01-28 00:24:36 +0530

    Add files via upload

---
+++ nested-if/Nestedif2.java
@@ -0,0 +1,21 @@
+class Nestedif2
+{
+public static void main(String agrs[])
+{
+
+        int a=10;
+        int b=20;
+       
+        if(a==10){
+            if(b==20){
+                System.out.println("True!");
+            }
+else{
+System.out.println("False!");
+}
+}
+else{
+System.out.println("Does not exit");
+}
+    }
+}
---
+++ nested-if/Nestedif3.java
@@ -0,0 +1,19 @@
+class Nestedif3
+{
+public static void main(String agrs[])
+{
+int x=10;
+int y=20;
+if(x==10)
+{
+System.out.println("x is equal to 10");
+if(x<y){
+System.out.println("y is greater than 10");
+}
+else
+{System.out.println("y is not greater than 10");}
+}
+else
+{
+System.out.println("ERROR");}
+}}
