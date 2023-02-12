2ace94754e16583b295fa204d4d1ec5dae0f2a1b
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-12 23:38:31 +0530

    Add files via upload

---
+++ Obj.java
@@ -0,0 +1,12 @@
+class Obj{
+void sum(int a, int b)
+{
+int add;
+add=a+b;
+System.out.println("The addition of 2 numbers: "+add);
+}
+public static void main(String args[])
+{
+Obj ob=new Obj();
+ob.sum(5,15);
+}}
