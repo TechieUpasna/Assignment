5286d6f0b1df164b9ffd12f8608099bb27200f8d
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-15 16:10:01 +0530

    Add files via upload

---
+++ DisplayMessage.java
@@ -0,0 +1,13 @@
+package com.cognizant.shapes;
+
+public class DisplayMessage {  // main class
+void printMessage() { //printMessage method
+	System.out.println("Hello My Message");
+}
+	public static void main(String[] args) { //main method
+		DisplayMessage dis=new DisplayMessage();  //object
+		dis.printMessage();
+
+	}
+
+}
