1337a419c4a99d36182fe23b5caefb033497e3f1
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-15 16:09:44 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+public class MyProgram {  //main class
+
+	public static void main(String[] args) {  //main method
+		System.out.println("My First Java Program");
+	}
+
+}
