0bfcdf6882cefa0e524c6efccc9ef31d6fce81c8
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-15 16:10:19 +0530

    Add files via upload

---
+++ AreaCalculator.java
@@ -0,0 +1,12 @@
+package com.cognizant.shapes;
+
+class Rectangle {
+void calculateArea() {  //calculateArea method
+	System.out.println("The Area of the rectangle is calculated using the formulaa length*breadth");
+}}
+public class AreaCalculator{  //main class
+	public static void main(String[] args) {  //main method
+		Rectangle rec=new Rectangle();  //object
+		rec.calculateArea();
+
+	}}
