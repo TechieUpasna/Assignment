cb387244c9b96462dc375733ab3d710f349ee965
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-22 16:22:58 +0530

    Add files via upload

---
+++ tax/EmployeeTax.java
@@ -0,0 +1,12 @@
+package com.cognizant.tax;
+
+public class EmployeeTax {
+
+	public static void main(String[] args) {
+	    TaxCalculator obj=new TaxCalculator();
+		obj.calculatorTax();
+		obj.deductTax();
+		obj.validatesalary();
+	}
+
+}
