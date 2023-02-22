0bb412478c1e68970402195dbf9030bf5baed838
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-02-22 16:47:34 +0530

    Update TaxCalculator.java

--- Com.Cognizant.tax/TaxCalculator.java
+++ Com.Cognizant.tax/TaxCalculator.java
@@ -1,23 +1,25 @@
 package com.cognizant.tax;
 
-public class TaxCalculator {
+public class TaxCalculator {  
+	//instance variable
 float basicSalary;
 boolean citizenship;
 int nettSalary;
 float tax;
-void calculatorTax() {
+void calculatorTax() {  //method to calculate tax
+	tax=30*basicSalary/100;
 	System.out.println("The Tax of the employee for the "+basicSalary+" is "+tax);
 }
-void deductTax() {
+void deductTax() {   //method to deduct tax
 	nettSalary=(int) (basicSalary-tax);
 	System.out.println("The nett Salary of the employee "+nettSalary);
 }
 void validatesalary() {
-	if(basicSalary>100000) {
+	if(basicSalary>100000) {  //if block
 		citizenship=true;
 		System.out.println("The salary and citizenship eligibility "+citizenship);
 	}
-	else {
+	else {  //else block
 		citizenship=false;
 		System.out.println("The salary and citizenship eligibility "+citizenship);
 	}
