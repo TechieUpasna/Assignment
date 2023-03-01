dd3f6c8e43fca16acb680a73e53f8772fdd3a518
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-03-01 15:13:48 +0530

    Add files via upload

---
+++ mypackage/Main.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person pr=new Person("Alice",25);
+
+	}
+
+}
---
+++ mypackage/Person.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Person {
+private String name;
+private int age;
+Person(String name, int age){
+
+System.out.println("Name: "+name+" "+"Age: "+age);
+}
+		
+
+}
